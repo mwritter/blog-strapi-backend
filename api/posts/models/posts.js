b72d0bf9460e4123897e9c8766a68427a85b040a
mwritter <mritter2283@gmail.com>
2022-01-10 09:36:14 -0500

    switching to postgres

--- api/posts/models/posts.js
+++ api/posts/models/posts.js
@@ -5,17 +5,41 @@
  * to customize this model
  */
 
+/**
+ * @method saveImages
+ * @description pulls all the images out of the markup
+ * credit to msoler75 https://forum.strapi.io/t/markdown-content-with-media-images-info-saved-in-another-field/9994
+ * @param {*} data
+ */
+const saveImages = async (data) => {
+  const regexp = /!\[[^\]]*\]\((.*?)\s*("(?:.*[^"])")?\s*\)/g;
+  const images = data.content.matchAll(regexp);
+  console.log(images);
+  // images is a field with multiple media, we only need to store id's of upload files
+  data.media = [];
+
+  for (const src of images) {
+    const img = await strapi.plugins.upload.services.upload.fetch({
+      url: src[1],
+    });
+    if (img && img.id) data.media.push(img.id);
+  }
+};
+
 const slugify = require("slugify");
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
       if (data.title) {
         data.slug = slugify(data.title.toLowerCase());
+        await saveImages(data);
       }
     },
     beforeUpdate: async (params, data) => {
+      console.log(data);
       if (data.title) {
         data.slug = slugify(data.title);
+        await saveImages(data);
       }
     },
   },
--- config/database.js
+++ config/database.js
@@ -5,12 +5,12 @@ module.exports = ({ env }) => {
       default: {
         connector: "bookshelf",
         settings: {
-          client: "mysql",
+          client: "postgres",
           host: env("DATABASE_HOST", "localhost"),
           port: env.int("DATABASE_PORT", 3306),
           database: env("DATABASE_NAME", "blogdatabase"),
-          username: env("DATABASE_USERNAME", "root"),
-          password: env("DATABASE_PASSWORD", "admin"),
+          username: env("DATABASE_USERNAME", "postgres"),
+          password: env("DATABASE_PASSWORD", "password"),
           ssl: env.bool("DATABASE_SSL", false),
         },
         options: {},
